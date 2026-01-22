faf663d912878078808eccf91d5ae25ac75fd250
rodrigomourelo <rodrigomourelo@gmail.com>
2026-01-22 19:39:24 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+// Esto permite recibir eventos de fondo
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || "Nuevo Pedido", {
+      body: data.body || "Tienes una nueva solicitud",
+      icon: "https://cdn-icons-png.flaticon.com/512/290/290441.png",
+      vibrate: [200, 100, 200]
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  // Básico: solo responde a las peticiones de red
   e.respondWith(fetch(e.request));
 });
